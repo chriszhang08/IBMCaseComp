3f86a432d92227b43d0db2c883b8acf0772434bf
chrzhang <chrzhang@umich.edu>
2022-10-10 02:45:51 -0400

    Initial Commit

---
+++ LocalBusinessMicrofinancer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalBusinessMicrofinancer
+//
+//  Created by Chris Zhang on 10/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocalBusinessMicrofinancer/LocalBusinessMicrofinancerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalBusinessMicrofinancerApp.swift
+//  LocalBusinessMicrofinancer
+//
+//  Created by Chris Zhang on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct LocalBusinessMicrofinancerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
