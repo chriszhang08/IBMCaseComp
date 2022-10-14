163e731b7020025629f37817b66d27ad44c1e326
Angelina Ilijevski <ailijev@umich.edu>
2022-10-14 16:35:03 -0400

    Initial Commit

---
+++ IBMCaseComp/IBMCaseCompApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBMCaseCompApp.swift
+//  IBMCaseComp
+//
+//  Created by Angelina Ilijevski on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct IBMCaseCompApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
